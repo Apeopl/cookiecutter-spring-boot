1fb44127705ffa6547dc32a146b2aa2432819be1
zhengjinlei <zhengjinlei@taijihuabao.com>
2020-01-07 19:12:37 +0800

    初始化

---
+++ {{cookiecutter.project_name}}/src/main/java/{{cookiecutter.package-first}}/{{cookiecutter.package-second}}/controller/HelloController.java
@@ -0,0 +1,15 @@
+package {{cookiecutter.package-first}}.{{cookiecutter.package-second}}.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String hello() {
+        return "hello spring boot!";
+    }
+
+}
